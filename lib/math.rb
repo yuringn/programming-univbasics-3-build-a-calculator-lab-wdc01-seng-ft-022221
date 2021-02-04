4db6a80cd1f1abfcc885e4fa7f65094221a5961b
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 18:43:22 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+num1 + num2
 end
+p addition(5,4)
 
 def subtraction(num1, num2)
-
+num1 - num2
 end
+p subtraction(10,5)
 
 def division(num1, num2)
-
+num1 / num2
 end
+division(50,5)
 
 def multiplication(num1, num2)
-
+num1 * num2
 end
+multiplication(4,30)
 
 def modulo(num1, num2)
-
+num1 % num2
 end
+modulo(34,5)
 
 def square_root(num)
-
+  Math.sqrt(num).to_i
 end
+p square_root(81)
